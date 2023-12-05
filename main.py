36626d3b45977a733452bcd1d76d5c7c8a7c4777
Ishan <ishan180901@gmail.com>
2023-12-05 21:46:05 +0530

    svd impl

--- main.py
+++ main.py
@@ -47,14 +47,24 @@ def psnr(mat1,mat2):
 
     ans = 0
     n = len(mat1)
-    m = len(mat2)
+    m = len(mat2[0])
     for i in range(n):
         for j in range(m):
             ans += (mat1[i][j] - mat2[i][j])**2
     mse = ans/(n*m)
-
-    num = (255**2)/mse
-    res = 10 * math.log(num,10)
+    maxi1 = 0
+    maxi2 = 0
+    # print(mat1,mat2)
+    for i in range(len(mat1)):
+        maxi1 = max(maxi1,max(mat1[i]))
+    for i in range(len(mat2)):
+        maxi2 = max(maxi2,max(mat2[i]))
+    # print(maxi1,maxi2)
+    maxi = max(maxi1,maxi2)
+    if mse == 0:
+        return 100
+    num = (maxi**2)/mse
+    res = 10 * math.log10(num)
     return res
 
 def dwt(coverImg,watermarkImg):
@@ -72,14 +82,31 @@ def dwt(coverImg,watermarkImg):
     cA , (cH , cV , cD) = coeffc
     # print(coeff)
     # print(cA , cH, cV , cD)
-    
+    coeffc2 = pywt.dwt2(cA,"haar")
+    cA2 , (cH2 , cV2 , cD2) = coeffc2
     watermarkImg = np.float64(watermarkImg)
     watermarkImg /= 255
-
+    
     a_c = 1
-    a_w = 0.1
-    coeffw = (a_c * cA + a_w * watermarkImg, (cH ,cV ,cD))
+    a_w = 0.4
+    coeffwm = pywt.dwt2(watermarkImg,"haar")
+    cWA , (cWH , cWV, cWD) = coeffwm
+    
+    U , D , VT = np.linalg.svd(cH2)
+    UW , DW , VTW = np.linalg.svd(cWD)
+    
+    new_D = ((a_c * D) + (a_w * DW))
+
+    new_cA = (U @ np.diag(new_D) @ VT)
+
+    # coeffw = (a_c * cA + a_w * watermarkImg, (cH ,cV ,cD))
+    # coeffw2 = (new_cA, (cH2 ,cV2 ,cD2))
+    coeffw2 = (cA2, (new_cA ,cV2 ,cD2))
+    temp = pywt.idwt2(coeffw2,"haar")
+    coeffw = (temp , (cH , cV , cD))
+
     watermarkedImg = pywt.idwt2(coeffw,"haar")
+    # print(watermarkedImg)
 
     # 
     # Attacks
@@ -95,8 +122,17 @@ def dwt(coverImg,watermarkImg):
 
     coeffWM = pywt.dwt2(watermarkedImg,"haar")
     hA , (hH , hV , hD) = coeffWM
+    
+    coeffWM2 = pywt.dwt2(hA,"haar")
+    hA2 ,(hH2 , hV2 , hD2) = coeffWM2
 
-    extracted = (hA-a_c*cA)/a_w
+    UWM , DWM , VTWM = np.linalg.svd(hH2)
+    DE = DWM - a_c*D
+    eA = (UW @ np.diag(DE) @ VTW)
+
+    # extracted = (hA-a_c*cA)/a_w
+    extracted = cWA , (cWH , cWV , eA)
+    extracted = pywt.idwt2(extracted,"haar")
     extracted *= 255
     extracted = np.uint8(extracted)
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,24 @@
-mg,watermarkedImg)
-    print(psnrval)
+ermarkedImg",watermarkedImg)
+    # psnrval = psnr(coverImg,watermarkedImg)
+    # print(psnrval)
+
+    # coeffWM = pywt.dwt2(watermarkedImg,"haar")
+    # hA , (hH , hV , hD) = coeffWM
+    
+    # coeffWM2 = pywt.dwt2(hA,"haar")
+    # hA2 ,(hH2 , hV2 , hD2) = coeffWM2
+
+    # UWM , DWM , VTWM = np.linalg.svd(hA2)
+    # DE = DWM - a_c*D
+    # eA = (UW @ np.diag(DE) @ VTW)
+
+    # # extracted = (hA-a_c*cA)/a_w
+    # extracted = eA , (cWH , cWV , cWD)
+    # extracted = pywt.idwt2(extracted,"haar")
+    # extracted *= 255
+    # extracted = np.uint8(extracted)
+
+    # cv.imshow("Extracted watermarkImg",extracted)
+
+    # nccval = ncc(watermarkImg,extracted)
+    # print(nccval)
